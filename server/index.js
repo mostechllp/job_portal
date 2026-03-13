df10ee9539c994f93cdcb5f082093f9c56d0144f
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2026-03-13 10:14:35 +0530

    update: frontend production url

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://jobportal-brown-delta.vercel.app"
+        ? "https://job-portal-client-two-ruddy.vercel.app"
         : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
